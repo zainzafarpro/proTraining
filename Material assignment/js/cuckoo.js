58b3eaabd1cf68927f286b90bc0a431f3c9346fd
Ahsan9212 <ahsan9212@yahoo.com>
2019-04-30 22:21:21 +0500

    after requested changes

--- Material assignment/js/cuckoo.js
+++ Material assignment/js/cuckoo.js
@@ -13,6 +13,24 @@ $(document).ready(function () {
       $(stars[i]).addClass('star-color');
     }
   });
+  $('.stars li').on('mouseover', function () {
+    var onStar = parseInt($(this).data('value'), 10); // The star currently mouse on
+
+    // Now highlight all the stars that's not after the current hovered star
+    $(this).parent().children('li.star').each(function (e) {
+      if (e < onStar) {
+        $(this).addClass('star-color');
+      }
+      else {
+        $(this).removeClass('star-color');
+      }
+    });
+
+  }).on('mouseout', function () {
+    $(this).parent().children('li.star').each(function (e) {
+      $(this).removeClass('star-color');
+    });
+  });
   if ($(window).width() < 767) {
     $('.sidenav').sidenav();
     $('.header-wrapper .fa-bars').click(function(){
